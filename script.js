c15e2b4118ad0ff388d6ef48ce8ff6cf21e7b69f
rla-xogud <tae.h.kim@outlook.com>
2021-11-03 14:08:22 -0500

    Remove functionality fixed

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let bookMap = new Map();
-// let BOOKOPEN = false;
 
 const Book ={
      init: function(title, author, pages, read){
@@ -7,20 +6,19 @@ const Book ={
         this.author = author;
         this.pages = pages;
         this.read = read;
-        // this.open = false;
     },
     info: function(){
-        let string = this.title +" by " + this.author + ", " + this.pages + " pages, ";
+        let string = this.title + "<br/><br/> By: " + this.author + "<br/><br/>" + this.pages + " Pages";
         if(this.read){
-            string += "read";
+            string += "<br/><br/>Read<br/>";
         }else{
-            string += "not read yet";
+            string += "<br/><br/>Haven't read yet<br/>";
         }
         return string;
     }
 }
 //popup for book info
-const modal = document.getElementById("bookModal");
+let modal = document.getElementById("bookModal");
 const addBookModal = document.getElementById("addBookModal");
 
 //closes modal if clicking outside it when it's open
@@ -33,11 +31,17 @@ window.onclick = function(event) {
 
 function closeModal(close){
     close.style.display = "none";
+    if(close == modal){
+        let new_modal = close.cloneNode(true);
+        close.parentNode.replaceChild(new_modal, close);
+        modal = new_modal;
+    }
 }
 const addBook = document.querySelector("#addBookBtn");
 addBook.addEventListener("click", ()=>{addBookToLibrary()});
 const BOOKCASE = document.querySelector("#bookcase");
 
+//Show modal that is used to add a book to the library
 function addBookToLibrary(){
     addBookModal.style.display = "block";
     document.body.style.overflow = "hidden";
@@ -53,34 +57,17 @@ function addBookToLibrary(){
             }
         }
     }
-    // let close = document.getElementsByClassName("addModalClose")[0];
-    // close.onclick = function(){
-    //     addBookModal.style.display = "none";
-    //     return;
-    // }
-    // let title = prompt("title");
-    // let author = prompt("author");
-    // //Use title + author to duplicate check because there can be books with same title by different authors
-    // let titleauthor = (title+author).toLowerCase();
-    // if(bookMap.get(titleauthor) != null){
-    //     alert("Book is already in library")
-    //     return;
-    // }
-    // let pages = prompt("pages");
-    // let read = prompt('read')
-    // let newBook = Object.create(Book);
-    // newBook.init(title,author,pages,read);
-    // bookMap.set(titleauthor, newBook);
-    // displayLibrary();
 }
+
+//Creates a book to add to library when information is submitted
 function submitBook(){
     //trim to remove leading and trailing spaces and replace multiple spaces with single
-    let title = document.getElementById("titleInput").replace(/\s+/g,' ').value.trim();
+    let title = document.getElementById("titleInput").value.replace(/\s+/g,' ').trim();
     if(title.length == 0){
         alert("Title can't be empty");
         return
     }
-    let author = document.getElementById("authorInput").replace(/\s+/g,' ').value.trim();
+    let author = document.getElementById("authorInput").value.replace(/\s+/g,' ').trim();
     if(author.length == 0){
         alert("Author can't be empty");
         return
@@ -90,7 +77,7 @@ function submitBook(){
         alert("Enter number of pages");
         return
     }
-    let read = document.getElementById("readInput").value;
+    let read = document.getElementById("readInput").checked;
 
     //title+author because you can have same title different author
     let titleauthor = (title + author).toLowerCase();
@@ -108,8 +95,6 @@ function submitBook(){
     document.getElementById("addBookForm").reset();
     
     closeModal(addBookModal);
-    //displayLibrary();
-    //alert("hello" + title  +author+ pages+ read)
 }
 
 function displayLibrary(){
@@ -119,6 +104,7 @@ function displayLibrary(){
     }
 }
 
+//Creates a new book to display on bookshelf
 function makeBook(book){
     let newBook = document.createElement('div')
     newBook.className = "book";
@@ -135,44 +121,65 @@ function makeBook(book){
     let hex = genHex();
     newBook.style.backgroundColor = hex;
     newBook.style.color = invertColor(hex, true);
-    let removeBtn = document.createElement("button")
-    removeBtn.className = "removeButton";
-    removeBtn.innerHTML = "x"
-    newBook.appendChild(removeBtn);
-    removeBtn.addEventListener('click', ()=>{removeBook(newBook)});
-    newBook.addEventListener('click', () => {openModal(newBook)});
+    newBook.addEventListener('click', () => {openModal(book, newBook)});
     return newBook;
 }
 
 //Opens pop-up modal to display more information about book that was selected
-function openModal(book){
+function openModal(book, bookDiv){
     modal.style.display = "block";
     document.body.style.overflow = "hidden";
-    
+    console.log(book)
+    console.log(bookDiv)
+
     //matches id of modal elements with classname of book elements and changes what's shown on modal
-    for(let child of modal.childNodes){
-        if(child.id == "bookContent"){
-            for(let info of child.childNodes){
-                //add onclick for x button
-                if(info.className == "modalClose"){
-                    info.addEventListener('click', () => closeModal(modal))
-                    continue;
-                }
-                for(let bookInfo of book.childNodes){
-                    if(info.id == bookInfo.className){
-                        info.innerHTML = bookInfo.innerHTML;
+    function updateModal(firstTime = true){
+        for(let child of modal.childNodes){
+            if(child.id == "bookContent"){
+                for(let info of child.childNodes){
+                    //add onclick for x button
+                    if(info.className == "modalClose"){
+                        info.addEventListener('click', () => closeModal(modal))
+                    }else if(info.id == "bookInfo"){
+                        info.innerHTML = book.info();
+                    }else if(info.className == "removeBook" && firstTime){
+                        info.addEventListener('click', function(event){
+                            if(event.target.className == "removeBook"){
+                                removeBook(bookDiv);
+                                //removes event listener so that on next remove correct div is assigned
+                                //this.removeEventListener('click', arguments.callee,false);
+                            }
+                        });
                     }
                 }
             }
         }
     }
+    updateModal();
+    
+    //Handling of read checkbox
+    let checkbox = document.getElementById("haveRead")
+    if(book.read){
+        checkbox.checked = true;
+    }else{
+        checkbox.checked = false;
+    }
+    checkbox.onchange = function(){
+        if(!checkbox.checked){
+            book.read = false;
+        }else{
+            book.read = true;
+        }
+        updateModal(false);
+    }
 }
 
 //Remove book from data structure and display
-function removeBook(book){
+function removeBook(book){//, button){
     book.parentNode.removeChild(book);
     let titleauthor = book.querySelector(".title").innerHTML + book.querySelector(".author").innerHTML;
     bookMap.delete(titleauthor.toLowerCase());
+    closeModal(modal)
 }
 
 //Generates 6 digit hex number to randomize color of books
@@ -225,51 +232,14 @@ function padZero(str, len) {
     return (zeros + str).slice(-len);
 }
 
-
-// /* Displays additional information about a book on hover 
-// *  (pages and if book has been read or not)
-// *  input: book
-// *
-// */
-// function showAdditionalDetails(book){ 
-    
-// //    let children = book.children;
-
-//     for(let child of book.children){
-//         if(child.className == "pages" || child.className == "read"|| 
-//         child.className == "removeButton"){
-//             child.style.display = "flex"
-//             //child.style["justify-content"] = "center"  
-//         }
-//         child.style["writing-mode"] = "horizontal-tb"
-//     }
-// }
-
-// /* Hides additional information about a book when not hovering over it 
-// *  (pages and if book has been read or not)
-// *  input: book
-// *
-// */
-// function hideAdditionalDetails(book){ 
-//     let children = book.children;
-
-//     for(let child of children){
-//         if(child.className == "pages" || child.className == "read" || 
-//             child.className == "removeButton"){
-//             child.style.display = "none"
-//         }
-//         child.style["writing-mode"] = "vertical-rl";
-//     }
-// }
-
 //Function only used to test bookcase
 function fillShelves(){
     for(let i = 0; i < 90; i++){
         let book = Object.create(Book);
-        book.init("a"+i,i+"b",i,i);
+        book.init("a"+i,i+"b",i, false);
         //myLibrary.push(book);
         bookMap.set("a"+i + i +"b", book)
     }
 }
-fillShelves();
+//fillShelves();
 displayLibrary();
